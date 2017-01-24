af7b09bc19853662c7aaa7f29a395a6d3a5cfaf7
marek.laskowski <marek.laskowski@senecacollege.ca>
2017-01-24 13:27:36 -0500

    sent a reply back to the MainActivity from SecondActivity

--- app/src/main/java/com/example/mareklaskowski/workshop2starter/SecondActivity.java
+++ app/src/main/java/com/example/mareklaskowski/workshop2starter/SecondActivity.java
@@ -1,7 +1,12 @@
 package com.example.mareklaskowski.workshop2starter;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button ok_button = (Button)findViewById(R.id.ok_button);
+        ok_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //let's pull the text out of the text box and put it into the intent as the "message" extra
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String userMessage = editText.getText().toString();
+                //create an intent to reply back to the MainActivity
+                Intent replyIntent = new Intent();
+                //set the result for this activity
+                replyIntent.putExtra("message", userMessage);
+                //post the reply intent, and dend the activity
+                setResult(Activity.RESULT_OK, replyIntent);
+            }
+        });
+
     }
 }
